24756ba12a4f6a76018290a5c028ce50830d09c8
dnebing <david.nebinger@liferay.com>
2016-09-13 01:38:28 -0400

    Added the rest of the portlet class implementation

--- modules/apps/filesystem-access-web/src/main/java/com/liferay/filesystemaccess/portlet/FilesystemAccessPortlet.java
+++ modules/apps/filesystem-access-web/src/main/java/com/liferay/filesystemaccess/portlet/FilesystemAccessPortlet.java
@@ -1,10 +1,17 @@
 package com.liferay.filesystemaccess.portlet;
 
+import com.liferay.filesystemaccess.api.FilesystemAccessService;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.Portlet;
-
-import org.osgi.service.component.annotations.Component;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import java.io.IOException;
 
 /**
  * class FilesystemAccessPortlet: This portlet is used to provide filesystem
@@ -22,8 +29,8 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.display-category=category.system.admin",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=false",
+		"javax.portlet.name=" + FilesystemAccessPortletKeys.FILESYSTEM_ACCESS,
 		"javax.portlet.display-name=Filesystem Access",
-		"javax.portlet.init-param.config-template=/configuration.jsp",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.resource-bundle=content.Language",
@@ -32,4 +39,41 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class FilesystemAccessPortlet extends MVCPortlet {
+
+	/**
+	 * render: Overrides the parent method to handle the injection of our
+	 * service as a render request attribute so it is available to all of the
+	 * jsp files.
+	 *
+	 * @param renderRequest The render request.
+	 * @param renderResponse The render response.
+	 * @throws IOException In case of error.
+	 * @throws PortletException In case of error.
+	 */
+	@Override
+	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
+
+		// set the service as a render request attribute
+		renderRequest.setAttribute(
+			Constants.ATTRIB_FILESYSTEM_SERVICE, _filesystemAccessService);
+
+		// invoke super class method to let the normal render operation run.
+		super.render(renderRequest, renderResponse);
+	}
+
+	/**
+	 * setFilesystemAccessService: Sets the filesystem access service instance to use.
+	 * @param filesystemAccessService The filesystem access service instance.
+	 */
+	@Reference(unbind = "-")
+	protected void setFilesystemAccessService(
+		final FilesystemAccessService filesystemAccessService) {
+
+		_filesystemAccessService = filesystemAccessService;
+	}
+
+	private FilesystemAccessService _filesystemAccessService;
+
+	private static final Log logger = LogFactoryUtil.getLog(
+		FilesystemAccessPortlet.class);
 }
